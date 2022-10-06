21d761e41f1ffca1bdf39bf3b6aadd193b754a9c
Vincent Banuelos <vincent.v.banuelos527@gmail.com>
2022-10-06 16:56:03 -0500

    adding work so far and wrangle

--- wrangle.py
+++ wrangle.py
@@ -39,17 +39,13 @@ def get_logs():
 def prepare_log():
     ''' This prepare function set the date column as index, drop unwanted columns    and set the start date and end date to date time format'''
     df = get_logs()
-    #change the date column to datetime
+    # Reassign the sale_date column to be a datetime type
     df.date = pd.to_datetime(df.date)
-    # set date column to index
-    df = df.set_index(df.date)
+    # Sort rows by the date and then set the index as that date
+    df = df.set_index("date").sort_index()
     #set the start_date and end_date column to datetime format
     df.start_date = pd.to_datetime(df.start_date)
     df.end_date = pd.to_datetime(df.end_date)
-    #split the endpoint into 4 different sections using / as sepeartor and concatenate to the dataframe
-    df= pd.concat([df, df.endpoint.str.split('/',3, expand = True)], axis=1)
-    # renaming the columns created after the split of endpoint columns as page 1, page 2,page 3, page 4 respectively
-    df.rename(columns={0:'page_1',1:'page_2',2:'page_3',3:'page_4'}, inplace = True)
     # data science program dataframe
     ds_df= df[(df.program_id == 3) & (df.cohort_name != 'Staff')]
     # web developers dataframe
